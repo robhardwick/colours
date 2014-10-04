63cfd821b94f0c5db9ed7a5445e71423bde97787
Rob Hardwick <robhardwick@gmail.com>
2014-10-04 02:25:27 +0100

    Refactor and add vertical stripe mode

--- colours.js
+++ colours.js
@@ -19,22 +19,22 @@ var Colours = (function() {
      */
     var Colour = function(values) {
         this.values = (values.length === 3) ? values : [0,0,0];
+    };
 
-        /**
-         * Return colour as "rgb(...)" format string
-         */
-        this.getRGB = function() {
-            return 'rgb(' + this.values.map(Math.round).join(',') + ')';
-        }.bind(this);
+    /**
+     * Return colour as "rgb(...)" format string
+     */
+    Colour.prototype.getRGB = function() {
+        return 'rgb(' + this.values.map(Math.round).join(',') + ')';
+    };
 
-        /**
-         * Get a new "random" colour, mixed with this colour
-         */
-        this.getRandom = function(mix) {
-            return new Colour(this.values.map(function (v) {
-                return ((Math.random() * 256) + v) / 2;
-            }));
-        }.bind(this);
+    /**
+     * Get a new "random" colour, mixed with this colour
+     */
+    Colour.prototype.getRandom = function(mix) {
+        return new Colour(this.values.map(function (v) {
+            return ((Math.random() * 256) + v) / 2;
+        }));
     };
 
     /**
@@ -42,6 +42,22 @@ var Colours = (function() {
      */
     var BLACK = new Colour([0, 0, 0]),
         WHITE = new Colour([255, 255, 255]);
+
+    /**
+     * Create and add DOM element
+     */
+    var createElement = function(tag, parent) {
+        return (parent || document.body).appendChild(document.createElement(tag));
+    };
+
+    /**
+     * Create and add button
+     */
+    var addButton = function(parent, label, obj, prop, value) {
+        var btn = createElement('button', parent);
+        btn.appendChild(document.createTextNode(label));
+        btn.onclick = function() { obj[prop] = value; };
+    };
 
     /**
      * Application class
@@ -54,75 +70,91 @@ var Colours = (function() {
         this.margin = opts.margin || 4;
         this.gutter = opts.gutter || 20;
         this.refresh = opts.refresh || 200;
+        this.frame = 0;
 
-        this.canvas = document.body.appendChild(document.createElement('canvas'));
+        // Create canvas
+        this.canvas = createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
 
-        var toolbar = document.body.appendChild(document.createElement('footer'));
-        var addButton = function(mode, label) {
-            var btn = toolbar.appendChild(document.createElement('button'));
-            btn.appendChild(document.createTextNode(label));
-            btn.onclick = function() { this.mode = mode; console.log(mode); }.bind(this);
-        }.bind(this);
-        addButton('colour', 'Colour');
-        addButton('monochrome', 'Monochrome');
+        // Create toolbar
+        var toolbar = createElement('footer');
+        for (var mode in this.getStyle) {
+            addButton(toolbar, mode, this, 'mode', mode);
+        }
 
-        /**
-         * Resize handler
-         */
-        window.onresize = function() {
-            // Fill viewport
-            this.canvas.style.width = '100%';
-            this.canvas.style.height = '100%';
-            this.canvas.width =  this.canvas.clientWidth;
-            this.canvas.height = this.canvas.clientHeight;
+        // Set canvas size and update on window resize
+        this.resize();
+        window.onresize = this.resize;
+    };
+    /**
+     * Resize handler
+     */
+    App.prototype.resize = function() {
+        // Fill viewport
+        this.canvas.style.width = '100%';
+        this.canvas.style.height = '100%';
+        this.canvas.width =  this.canvas.clientWidth;
+        this.canvas.height = this.canvas.clientHeight;
 
-            // Calcuate cell size
-            this.cellX = ((this.canvas.width - (this.margin * 2)) / this.cols) - this.margin,
-            this.cellY = ((this.canvas.height - (this.margin * 2) - this.gutter) / this.rows) - this.margin;
-        }.bind(this)();
+        // Calcuate cell size
+        this.cellX = ((this.canvas.width - (this.margin * 2)) / this.cols) - this.margin,
+        this.cellY = ((this.canvas.height - (this.margin * 2) - this.gutter) / this.rows) - this.margin;
+    };
 
-        /**
-         * Cell style handlers
-         */
-        this.getStyle = {
-            // Gradient with random from/to colours mixed with the base colour
-            colour: function(x, y) {
-                var style = this.ctx.createLinearGradient(x, y, x + this.cellX, y + this.cellY);
-                style.addColorStop(0, this.colour.getRandom().getRGB());
-                style.addColorStop(1, this.colour.getRandom().getRGB());
-                return style;
-            }.bind(this),
+    /**
+     * Cell style handlers
+     */
+    App.prototype.getStyle = {
 
-            // Random on or off
-            monochrome: function() {
-                return (Math.random() > 0.5) ? this.colour.getRGB() : WHITE.getRGB();
-            }.bind(this),
-        };
-
-        /**
-         * Main loop
-         */
-        this.run = function() {
-            // Queue next frame
-            requestAnimationFrame(this.run, this.canvas);
-
-            // Render every X ms
-            var now = new Date().getTime();
-            if (this.time && (now - this.time) < this.refresh) {
-                return;
+        // Vertical stripes
+        stripes: function(x, y, frame) {
+            this._stripe = !this._stripe;
+            if (((this._stripe % 2) === 0 && (frame % 2) === 0) ||
+                ((this._stripe % 2) !== 0 && (frame % 2) !== 0)) {
+                return this.colour.getRGB();
             }
-            this.time = now;
+            return WHITE.getRGB();
+        },
 
-            // Draw cells
-            for (var y = this.margin; (y + this.cellY + this.gutter) < this.canvas.height; y += this.cellY + this.margin) {
-                for (var x = this.margin; (x + this.cellX) < this.canvas.width; x += this.cellX + this.margin) {
-                    this.ctx.fillStyle = this.getStyle[this.mode](x, y);
-                    this.ctx.fillRect(x, y, this.cellX, this.cellY);
-                }
+        // Random on or off
+        monochrome: function() {
+            return (Math.random() > 0.5) ? this.colour.getRGB() : WHITE.getRGB();
+        },
+
+        // Gradient with random from/to colours mixed with the base colour
+        colour: function(x, y) {
+            var style = this.ctx.createLinearGradient(x, y, x + this.cellX, y + this.cellY);
+            style.addColorStop(0, this.colour.getRandom().getRGB());
+            style.addColorStop(1, this.colour.getRandom().getRGB());
+            return style;
+        },
+
+    };
+
+    /**
+     * Main loop
+     */
+    App.prototype.run = function() {
+        // Queue next frame
+        requestAnimationFrame(this.run.bind(this), this.canvas);
+
+        // Render every X ms
+        var now = new Date().getTime();
+        if (this.time && (now - this.time) < this.refresh) {
+            return;
+        }
+        this.time = now;
+
+        // Draw cells
+        for (var y = this.margin; (y + this.cellY + this.gutter) < this.canvas.height; y += this.cellY + this.margin) {
+            for (var x = this.margin; (x + this.cellX) < this.canvas.width; x += this.cellX + this.margin) {
+                this.ctx.fillStyle = this.getStyle[this.mode].call(this, x, y, this.frame);
+                this.ctx.fillRect(x, y, this.cellX, this.cellY);
             }
-        }.bind(this);
-    }
+        }
+
+        this.frame++;
+    };
 
     return {Colour: Colour, App: App};
 
