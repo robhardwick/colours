a704c19a00033dee564d9c503d5d1ca8b75474a9
Rob Hardwick <robhardwick@gmail.com>
2014-10-08 20:25:29 +0100

    Minor fixes and renames

--- colours.js
+++ colours.js
@@ -8,11 +8,6 @@ var Colours = (function() {
             window.mozRequestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
             window.msRequestAnimationFrame;
-        cancelAnimationFrame =
-            window.cancelAnimationFrame ||
-            window.mozCancelAnimationFrame ||
-            window.webkitCancelAnimationFrame ||
-            window.msCancelAnimationFrame;
 
     /**
      * Colour class
@@ -63,7 +58,7 @@ var Colours = (function() {
      * Application class
      */
     var App = function(opts) {
-        this.mode = opts.mode || 'colour';
+        this.mode = opts.mode || 'gradient';
         this.colour = opts.colour || BLACK;
         this.rows = opts.rows || 10;
         this.cols = opts.cols || 10;
@@ -86,20 +81,34 @@ var Colours = (function() {
         this.resize();
         window.onresize = this.resize;
     };
+
     /**
      * Resize handler
      */
     App.prototype.resize = function() {
+        if (!this.canvas) {
+            return;
+        }
+
         // Fill viewport
         this.canvas.style.width = '100%';
         this.canvas.style.height = '100%';
-        this.canvas.width =  this.canvas.clientWidth;
+        this.canvas.width = this.canvas.clientWidth;
         this.canvas.height = this.canvas.clientHeight;
 
         // Calcuate cell size
-        this.cellX = ((this.canvas.width - (this.margin * 2)) / this.cols) - this.margin,
+        this.cellX = ((this.canvas.width - (this.margin * 2)) / this.cols) - this.margin;
         this.cellY = ((this.canvas.height - (this.margin * 2) - this.gutter) / this.rows) - this.margin;
     };
+
+    /**
+     * Stripe style helper
+     */
+    function stripe(coord, frame, size, margin) {
+        var c = Math.round((coord - margin) / (size + margin)) % 2,
+            f = frame % 2;
+        return ((c === 0 && f === 0) || (c !== 0 && f !== 0));
+    }
 
     /**
      * Cell style handlers
@@ -107,22 +116,28 @@ var Colours = (function() {
     App.prototype.getStyle = {
 
         // Vertical stripes
-        stripes: function(x, y, frame) {
-            this._stripe = !this._stripe;
-            if (((this._stripe % 2) === 0 && (frame % 2) === 0) ||
-                ((this._stripe % 2) !== 0 && (frame % 2) !== 0)) {
+        vstripes: function(x, y, frame) {
+            if (stripe(x, frame, this.cellX, this.margin)) {
+                return this.colour.getRGB();
+            }
+            return WHITE.getRGB();
+        },
+
+        // Horizontal stripes
+        hstripes: function(x, y, frame) {
+            if (stripe(y, frame, this.cellY, this.margin)) {
                 return this.colour.getRGB();
             }
             return WHITE.getRGB();
         },
 
         // Random on or off
-        monochrome: function() {
+        random: function() {
             return (Math.random() > 0.5) ? this.colour.getRGB() : WHITE.getRGB();
         },
 
         // Gradient with random from/to colours mixed with the base colour
-        colour: function(x, y) {
+        gradient: function(x, y) {
             var style = this.ctx.createLinearGradient(x, y, x + this.cellX, y + this.cellY);
             style.addColorStop(0, this.colour.getRandom().getRGB());
             style.addColorStop(1, this.colour.getRandom().getRGB());
